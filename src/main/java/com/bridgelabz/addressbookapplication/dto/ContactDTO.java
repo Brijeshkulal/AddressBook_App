183eb6ec5bfef32735a3f491f665e6a11f6bc4d6
“Brijesh” <brijeshkulal5@gmail.com>
2021-12-17 15:46:05 +0530

    Added dto and model to addressbook

--- src/main/java/com/bridgelabz/addressbookapplication/dto/ContactDTO.java
+++ src/main/java/com/bridgelabz/addressbookapplication/dto/ContactDTO.java
@@ -10,12 +10,6 @@ public class ContactDTO {
     public String zip;
     public String phone;
 	
-    @Override
-	public String toString() {
-		return "ContactDTO [firstName=" + firstName + ", lastName=" + lastName + ", address=" + address + ", state="
-				+ state + ", city=" + city + ", zip=" + zip + ", phone=" + phone + "]";
-	}
-
 	public ContactDTO(String firstName, String lastName, String address, String state, String city, String zip,
 			String phone) {
 		super();
@@ -27,5 +21,11 @@ public class ContactDTO {
 		this.zip = zip;
 		this.phone = phone;
 	}
+	
+	  @Override
+		public String toString() {
+			return "ContactDTO [firstName=" + firstName + ", lastName=" + lastName + ", address=" + address + ", state="
+					+ state + ", city=" + city + ", zip=" + zip + ", phone=" + phone + "]";
+		}
 
 }
