64d1daac36730cf811c0b2fb2d339e283dcb7d5e
“Brijesh” <brijeshkulal5@gmail.com>
2021-12-19 17:38:26 +0530

    added validation field for name,phonenumber

--- src/main/java/com/bridgelabz/addressbookapplication/dto/ContactDTO.java
+++ src/main/java/com/bridgelabz/addressbookapplication/dto/ContactDTO.java
@@ -1,5 +1,7 @@
 package com.bridgelabz.addressbookapplication.dto;
 
+import javax.validation.constraints.Pattern;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.ToString;
@@ -9,12 +11,20 @@ import lombok.ToString;
 @ToString
 public class ContactDTO {
 	
-	public String firstName;
+	@Pattern(regexp = "^[A-Z]{1}[a-z]{2,}$", message = "First name Invalid")
+    public String firstName;
+
+    @Pattern(regexp = "^[A-Z]{1}[a-z]{2,}$", message = "Last name Invalid")
     public String lastName;
+
     public String address;
+
     public String state;
     public String city;
-    public String zip;
-    public String phone;
 
+    @Pattern(regexp = "^[0-9]{3}\\s{0,1}[0-9]{3}$", message = "Zip Code Invalid")
+    public String zip;
+
+    @Pattern(regexp = "^([\\+]?\\d{2})?[\\s]?\\d{10}$", message = "Phone Invalid")
+    public String phone;
 }
