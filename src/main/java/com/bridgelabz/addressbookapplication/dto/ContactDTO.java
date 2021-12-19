4a6a94bcc36b6d3b4a4f5c6675f97320063223e2
“Brijesh” <brijeshkulal5@gmail.com>
2021-12-19 16:40:17 +0530

    added lambook annotation instead of getters and setters

--- src/main/java/com/bridgelabz/addressbookapplication/dto/ContactDTO.java
+++ src/main/java/com/bridgelabz/addressbookapplication/dto/ContactDTO.java
@@ -1,5 +1,12 @@
 package com.bridgelabz.addressbookapplication.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@ToString
 public class ContactDTO {
 	
 	public String firstName;
@@ -9,23 +16,5 @@ public class ContactDTO {
     public String city;
     public String zip;
     public String phone;
-	
-    @Override
-	public String toString() {
-		return "ContactDTO [firstName=" + firstName + ", lastName=" + lastName + ", address=" + address + ", state="
-				+ state + ", city=" + city + ", zip=" + zip + ", phone=" + phone + "]";
-	}
-
-	public ContactDTO(String firstName, String lastName, String address, String state, String city, String zip,
-			String phone) {
-		super();
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.address = address;
-		this.state = state;
-		this.city = city;
-		this.zip = zip;
-		this.phone = phone;
-	}
 
 }
