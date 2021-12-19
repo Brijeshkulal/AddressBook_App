af8cef453ebdbb0205d36d6832c60a67f0144462
“Brijesh” <brijeshkulal5@gmail.com>
2021-12-19 16:56:42 +0530

    Added the log application details

--- src/main/java/com/bridgelabz/addressbookapplication/AddressbookapplicationApplication.java
+++ src/main/java/com/bridgelabz/addressbookapplication/AddressbookapplicationApplication.java
@@ -3,11 +3,15 @@ package com.bridgelabz.addressbookapplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+
 @SpringBootApplication
+@Slf4j
 public class AddressbookapplicationApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AddressbookapplicationApplication.class, args);
+		log.info("Employee Payroll App Started.");
 	}
 
 }
